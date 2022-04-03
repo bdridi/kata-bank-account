b1d94ca3e214cc44df873e494d1df25361d6fda2
Bilel DRIDI <bilel.dridi.ext@paylib.fr>
2022-04-03 23:05:53 +0200

    main class and README.md

--- README.md
+++ README.md
@@ -4,3 +4,34 @@ Bank account kata simulate a simple personal bank account experience :
 · Deposit and Withdrawal
 · Account statement (date, amount, balance)
 · Statement printing
+
+## Built with
+
+- Kotlin
+- Maven
+
+## How to launch
+
+- Create an account and some transactions in file `main.kt` and the print statement as the example below :
+
+
+````kotlin
+    // create an account
+    val account = Account(number = UUID.randomUUID().toString(), owner = "Chuck Norris", currency = CurrencyUnit.EUR)
+
+    // create transactions
+
+    accountConsole.deposit(account, Money.parse("EUR 100"))
+    accountConsole.deposit(account, Money.parse("EUR 300"))
+    accountConsole.withdrawal(account, Money.parse("EUR 100"))
+    accountConsole.deposit(account, Money.parse("EUR 20"))
+    accountConsole.withdrawal(account, Money.parse("EUR 40"))
+    accountConsole.withdrawal(account, Money.parse("EUR 45"))
+
+    // print statement
+
+    println(accountConsole.printStatement(account))
+
+````
+
+Now run the main class `src/main/kotlin/main.kt`
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,31 @@
+import io.shodo.application.AccountConsole
+import io.shodo.domain.AccountService
+import io.shodo.domain.Clock
+import io.shodo.domain.model.Account
+import io.shodo.infrastructure.InMemoryTransactionRepository
+import org.joda.money.CurrencyUnit
+import org.joda.money.Money
+import java.util.*
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    // Config
+    val transactionRepository = InMemoryTransactionRepository()
+    val accountService = AccountService(transactionRepository, Clock())
+    val accountConsole = AccountConsole(accountService)
+
+    // create an account
+    val account = Account(number = UUID.randomUUID().toString(), owner = "Chuck Norris", currency = CurrencyUnit.EUR)
+
+    // create transactions
+
+    accountConsole.deposit(account, Money.parse("EUR 100"))
+    accountConsole.deposit(account, Money.parse("EUR 300"))
+    accountConsole.withdrawal(account, Money.parse("EUR 100"))
+    accountConsole.deposit(account, Money.parse("EUR 20"))
+    accountConsole.withdrawal(account, Money.parse("EUR 40"))
+    accountConsole.withdrawal(account, Money.parse("EUR 45"))
+
+    // print statement
+
+    println(accountConsole.printStatement(account))
 }
